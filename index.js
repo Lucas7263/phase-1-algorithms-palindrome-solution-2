a9e3637d9fdd4465d7a9df88f46018ac1d22b50e
Lucas <lucaswoodyard@yahoo.com>
2022-08-21 15:29:11 -0500

    commit tree 35e72d81a1ad7af08d61e6d60123a99c67217e02

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function isPalindrome(word) {
   // Write your algorithm here
+    for (let i = 0; i < word.length / 2; i++) {
+      const j = word.length - 1 - i;
+      if (word[i] !== word[j]) return false;
+    }
+    return true;
 }
 
 /* 
@@ -16,10 +21,14 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("dad"))
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("fragile"));
 }
 
 module.exports = isPalindrome;
